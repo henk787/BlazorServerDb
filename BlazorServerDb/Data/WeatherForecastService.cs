656c91beed22edce0443081587875f7f1f3c2940
Henk Holterman <HenkH@4dotnet.nl>
2023-08-31 17:06:20 +0200

    changes service (Transient, Factory)

--- BlazorServerDb/Data/WeatherForecastService.cs
+++ BlazorServerDb/Data/WeatherForecastService.cs
@@ -1,20 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+
 namespace BlazorServerDb.Data
 {
     public class WeatherForecastService
     {
+        private readonly DbContextFactory<ApplicationDbContext> _appDbContextFactory;
+
+        public WeatherForecastService(DbContextFactory<ApplicationDbContext> appDbContextFactory)
+        {
+            _appDbContextFactory = appDbContextFactory;
+        }
+
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
+
+        public async Task<int> AddForeCasts(DateOnly startDate)
         {
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var newForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = startDate.AddDays(index).ToDateTime(TimeOnly.MinValue),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            }).ToArray());
+            }).ToArray();
+
+            await using var dbContext = await _appDbContextFactory.CreateDbContextAsync();
+
+            dbContext.WeatherForecasts.AddRange(newForecasts);
+
+            return await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<WeatherForecast[]> GetForecastAsync()
+        {
+            await using var dbContext = await _appDbContextFactory.CreateDbContextAsync();
+
+            var results = await dbContext.WeatherForecasts
+                .OrderByDescending(w => w.Date)
+                .ToArrayAsync();
+
+            return results;
         }
     }
 }
